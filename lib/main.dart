b14013496c40c3a87b3e9eefb53417c6b966aaff
Vijay <vijaythevirtue@gmail.com>
2020-06-15 22:52:54 +0530

    Added all new Features | Stable

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:audioplayers/audio_cache.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
@@ -27,6 +29,9 @@ AudioCache audioCache;
 AudioPlayer audioPlayer;
 Duration _duration = new Duration();
 Duration _position = new Duration();
+double _sigmaX = 6.5; // from 0-10
+double _sigmaY = 6.5; // from 0-10
+double _opacity = 20.1; // from 0-1.0
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
@@ -53,47 +58,62 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text("Audio Player in Flutter"),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Container(
-              child: FlatButton(
-                onPressed: () {
-                  audioCache.play("songs1.mp3");
-                },
-                child: Text("play"),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/balaji.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: _sigmaX, sigmaY: _sigmaY),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 150,
               ),
-            ),
-            Container(
-              child: FlatButton(
-                onPressed: () {
-                  audioPlayer.pause();
-                },
-                child: Text("pause"),
+              Container(
+                child: FlatButton(
+                  onPressed: () {
+                    audioCache.play("songs1.mp3");
+                  },
+                  child: Text("play"),
+                ),
               ),
-            ),
-            Container(
-              child: FlatButton(
-                onPressed: () {
-                  audioPlayer.stop();
-                },
-                child: Text("stop"),
+              Container(
+                child: FlatButton(
+                  onPressed: () {
+                    audioPlayer.pause();
+                  },
+                  child: Text("pause"),
+                ),
               ),
-            ),
-            Slider(
-              activeColor: Colors.blue,
-              inactiveColor: Colors.pink,
-              value: _position.inSeconds.toDouble(),
-              // min: 0.0,
-              max: _duration.inSeconds.toDouble(),
-              onChanged: (double value) {
-                setState(() {
-                  seekToSecond(value.toInt());
-                  value = value;
-                });
-              },
-            ),
-          ],
+              Container(
+                child: FlatButton(
+                  onPressed: () {
+                    audioPlayer.stop();
+                  },
+                  child: Text("stop"),
+                ),
+              ),
+              SizedBox(
+                height: 35,
+              ),
+              Slider(
+                activeColor: Colors.blue,
+                inactiveColor: Colors.grey,
+                value: _position.inSeconds.toDouble(),
+                // min: 0.0,
+                max: _duration.inSeconds.toDouble(),
+                onChanged: (double value) {
+                  setState(() {
+                    seekToSecond(value.toInt());
+                    value = value;
+                  });
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
